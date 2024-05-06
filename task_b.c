16d0ed5046b98947d7867d556cc35c83c59beca6
Yufu-W <160229997+Yufu-W@users.noreply.github.com>
2024-05-06 14:57:41 +0100

    Add files via upload

---
+++ task_b.c
@@ -0,0 +1,23 @@
+#define _USE_MATH_DEFINES //It needs to be placed before match before defining parameters using matches such as M6PI
+#include <math.h>
+#include <stdio.h>
+int main(void)
+{
+	double height = 0;
+	double volumne = 0;
+	printf("Enter height: ");
+	scanf("%lf", &height);
+	
+	// Your code below here
+	if (height <= 0)
+		height = -height;
+	const double radius = 2.2;
+	volumne = M_PI * radius * radius * height;
+	
+	
+
+	// Do not edit below here
+	printf("Volume: %.2f", volumne);
+
+	return 0;
+}
